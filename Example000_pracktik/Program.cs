0651d1b49bced5081b76b9b4cc3f1ca910ffc6c0
Nataly <natali.nezvanova02@mail.ru>
2023-12-11 18:50:24 +0600

    add redacktion

---
+++ Example000_pracktik/Program.cs
@@ -0,0 +1,20 @@
+﻿string Method4(int count, string text)
+{
+    string result = String.Empty;
+for( int i = 0; i < count; i++)
+    {
+        result = result + text;
+    }
+    return result;
+}
+string res = Method4(10, "z");
+//Console.WriteLine(res);
+
+for(int i = 2; i <= 10; i++)
+{
+    for(int j = 2; j <= 10; j++)
+    {
+        Console.Write($"{i} х {j} = {i * j}\t");
+    }
+    Console.WriteLine();
+}

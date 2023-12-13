3288b0f0f5f34f1446c78b877270e2c12d7af88d
Nataly <natali.nezvanova02@mail.ru>
2023-12-13 19:12:37 +0600

    Example000_pracktik

--- Example000_pracktik/Program.cs
+++ Example000_pracktik/Program.cs
@@ -14,7 +14,56 @@ for(int i = 2; i <= 10; i++)
 {
     for(int j = 2; j <= 10; j++)
     {
-        Console.Write($"{i} х {j} = {i * j}\t");
+       // Console.Write($"{i} х {j} = {i * j}\t");
+    }
+   // Console.WriteLine();
+}
+
+
+
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
     }
     Console.WriteLine();
 }
+
+
+
+void SelectionSort(int[] array)
+{
+    int count = array.Length;
+
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int maxPosition = i;
+
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] > array[maxPosition]) 
+            {
+                maxPosition = j;
+            }
+        }
+        int temporary = array[i];
+        array[i] = array[maxPosition];
+        array[maxPosition] = temporary;
+    }
+    
+}
+
+
+int[] arr = {1, 1, 4, 6, 8, 9, 3, 6, 5};
+
+//PrintArray(arr);
+//SelectionSort(arr);
+
+//PrintArray(arr);
+
+
+

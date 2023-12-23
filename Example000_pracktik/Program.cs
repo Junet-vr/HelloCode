1b4c89279c0070302d04619de2d9a9ae9421fcd7
Nataly <natali.nezvanova02@mail.ru>
2023-12-23 16:11:43 +0600

    Example000_pracktik

--- Example000_pracktik/Program.cs
+++ Example000_pracktik/Program.cs
@@ -1,23 +1,23 @@
-﻿string Method4(int count, string text)
-{
-    string result = String.Empty;
-for( int i = 0; i < count; i++)
-    {
-        result = result + text;
-    }
-    return result;
-}
-string res = Method4(10, "z");
-//Console.WriteLine(res);
+﻿// string Method4(int count, string text)
+// {
+//     string result = String.Empty;
+// for( int i = 0; i < count; i++)
+//     {
+//         result = result + text;
+//     }
+//     return result;
+// }
+// string res = Method4(10, "z");
+// //Console.WriteLine(res);
 
-for(int i = 2; i <= 10; i++)
-{
-    for(int j = 2; j <= 10; j++)
-    {
-       // Console.Write($"{i} х {j} = {i * j}\t");
-    }
-   // Console.WriteLine();
-}
+// for(int i = 2; i <= 10; i++)
+// {
+//     for(int j = 2; j <= 10; j++)
+//     {
+//        Console.Write($"{i} х {j} = {i * j}\t");
+//     }
+//    Console.WriteLine();
+// }
 
 
 
@@ -45,7 +45,7 @@ void SelectionSort(int[] array)
 
         for (int j = i + 1; j < array.Length; j++)
         {
-            if (array[j] > array[maxPosition]) 
+            if (array[j] < array[maxPosition]) 
             {
                 maxPosition = j;
             }
@@ -60,10 +60,10 @@ void SelectionSort(int[] array)
 
 int[] arr = {1, 1, 4, 6, 8, 9, 3, 6, 5};
 
-//PrintArray(arr);
-//SelectionSort(arr);
+PrintArray(arr);
+SelectionSort(arr);
 
-//PrintArray(arr);
+PrintArray(arr);
 
 
 
